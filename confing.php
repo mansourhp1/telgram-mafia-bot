981440ff9c2c1a8392dd5d46ac51faf526c6c841
mansourhp1 <mansour2hajipoor@gmail.com>
2025-11-30 10:08:14 -0800

    Create confing.php

---
+++ confing.php
@@ -0,0 +1,7 @@
+<?php
+// توکن رباتت رو اینجا بذار
+$botToken = '8336683539:AAE4XQCs-h33fcdrsBQ7iJZ0Wvu9X2c4MYE';
+
+// آدرس URL که برای Webhook استفاده می‌کنی (اگر داری)
+$webhookUrl = 'https://yourdomain.com/bot.php';
+?>
